3f47a4de5823a0cc57139f76d73af50bd944e09e
Sébastien Joly <joly.sebastien@gmail.com>
2015-09-08 07:20:36 +0200

    Create boutonJaune.lua

---
+++ VDBulbLed/boutonJaune.lua
@@ -0,0 +1,4 @@
+local BulbID = fibaro:get(fibaro:getSelfId(), "IPAddress")
+HC2 = Net.FHttp("127.0.0.1", 11111)
+jtable = '{"properties":{"parameters":[{"id":38,"size":4,"value":51},{"id":37,"size":4,"value":1107296510}]}}'
+HC2:PUT("/api/devices/".. BulbID,jtable)
